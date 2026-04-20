a98a3b1dc5aa657a1520dd8fe74d3598500f0bb8
Lennox Lewis <lennylewis6a6y@gmail.com>
2026-04-20 10:02:38 +0300

    Hoodie Haven Startup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hoodie from './components/hoodie'
+
+const App = () => {
+  return (
+    <div>
+      <Hoodie />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/hoodie.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const hoodie = () => {
+  return (
+    <div className='Hoodie-Component'>
+        <img src="/assets/Red-black hoodie.png" alt="" />
+        <h2>Red-black Hoodie</h2>
+        <p>Luxurious Red-black Hoodie Made from 100% Cotton</p>
+        <span>$100</span>
+        <button>Add to Cart</button>
+    </div>
+  )
+}
+
+export default hoodie
